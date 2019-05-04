296f38c0f2892b91ed470a048b77446f10c73b0a
Chris Philbin <chris@chrisphilbin.net>
2019-05-04 19:51:49 -0400

    Setting up relationship between landlords and reviews

--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -1,13 +1,17 @@
 class Landlord < ApplicationRecord
 	has_many :properties
 	has_many :tenants, through: :properties
-	has_many :reviews, through: :tenants
+	has_many :reviews, through: :properties
 
-	def self.how_many_properties?
-		inclues(:properties).where(properties: {landlord_id: params[:id]})
+	def self.number_of_properties
+		inclues(:properties).where(properties: {landlord_id: params[:id]}).count
 	end
 
 	def self.overall_rating
 		
+	end
+
+	def self.number_of_tenants
+
 	end
 end
---
+++ db/schema.rb
@@ -0,0 +1,52 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_05_04_184624) do
+
+  create_table "landlord_properties", force: :cascade do |t|
+    t.integer "landlord_id"
+    t.integer "property_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "landlords", force: :cascade do |t|
+    t.text "firstname"
+    t.text "lastname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "properties", force: :cascade do |t|
+    t.text "address1"
+    t.text "address2"
+    t.text "city"
+    t.text "state"
+    t.integer "zip"
+    t.text "propertytype"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "contents"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tenants", force: :cascade do |t|
+    t.text "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
