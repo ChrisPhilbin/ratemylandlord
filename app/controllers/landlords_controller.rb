4056a31082e081214dedc5e9b4b7b802b87dea0a
Chris Philbin <chris@chrisphilbin.net>
2019-05-04 14:29:14 -0400

    Building out controller functionality and basic model db quearies for landlords

--- app/controllers/landlords_controller.rb
+++ app/controllers/landlords_controller.rb
@@ -1,2 +1,29 @@
 class LandlordsController < ApplicationController
+
+	def new
+		@landlord = Landlord.new
+	end
+
+	def show
+		@landlord = Landlord.find(params[:id])
+	end
+
+	def create
+		@lanlord = Landlord.new(landlord_params)
+		if @landlord.save
+			redirect_to @landlord
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@landlord = Landlord.find(params[:id])
+	end
+
+	private
+
+	def landlord_params
+		params.require(:landlord).permit(:firstname, :lastname)
+	end
 end
--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -1,5 +1,5 @@
 class Landlord < ApplicationRecord
 	has_many :properties
 	has_many :tenants, through: :properties
-	#test
+
 end
