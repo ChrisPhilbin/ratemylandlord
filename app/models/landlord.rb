7f7a089a6697ae68ed6a835ac89616268ae4b2d4
Chris Philbin <chris@chrisphilbin.net>
2019-05-04 14:31:40 -0400

    Implementing basic model functionality for landlord objects

--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -2,4 +2,11 @@ class Landlord < ApplicationRecord
 	has_many :properties
 	has_many :tenants, through: :properties
 
+	def self.how_many_properties?
+		inclues(:properties).where(properties: {landlord_id: params[:id]})
+	end
+
+	def self.overall_rating
+		
+	end
 end
