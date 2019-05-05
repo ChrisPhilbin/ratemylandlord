02e0a4add8004fee1318dbe42cc0732c38086c32
Chris Philbin <chris@chrisphilbin.net>
2019-05-05 13:58:25 -0400

    Implementing basic setter and getter methods for properties

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,2 +1,21 @@
 class PropertiesController < ApplicationController
+
+	def new
+		@property = Property.new
+	end
+
+	def create
+		@property = Property.new(property_params)
+		if @property.save
+			redirect_to @property
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def property_params
+		params.require(:property).permit(:address1, :address2, :city, :state, :zip, :propertytype)
+	end
 end
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -2,4 +2,8 @@ class Tenant < ApplicationRecord
 	has_many :properties
 	has_many :landlords, through: :properties
 	has_many :reviews
+
+	def new
+		@tenant = Tenant.new
+	end
 end
