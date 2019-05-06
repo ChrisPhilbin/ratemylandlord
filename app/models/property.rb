e433605004094bad312006c1d9660b8e0dfa8301
Chris Philbin <chris@chrisphilbin.net>
2019-05-06 08:13:56 -0400

    Adding more basic validations to the property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,12 @@
 class Property < ApplicationRecord
 	has_many :landlord_properties
 	has_many :reviews
+	validates :address1, presence: true
+	validates :city, presence: true
+	validates :state, presence: true
+	validates :zip, presence: true
 	validates :zip, numericality: true
-	validates :zip, length: { is: 5 }
+	validates :zip, length: { maximum: 5,
+    too_long: "Zip codes can only be %{count} numbers long" }
+    validates :propertytype, presence: true
 end
