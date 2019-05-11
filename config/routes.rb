128a04ce835bfcf4c7600f1848b9529c06a9f930
Chris Philbin <chris@chrisphilbin.net>
2019-05-11 09:09:10 -0400

    Stubbing out nested routes - properties are nested below landlords

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :reviews
   resources :tenants
-  resources :properties
-  resources :landlords
+  resources :landlords do
+  	resources :properties
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
